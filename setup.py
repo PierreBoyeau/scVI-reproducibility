0d3bf2b50e841099d9dbb0640a5c481e70393cf9
Pierre Boyeau <pierre.boyeau@gmail.com>
2019-08-07 11:42:36 -0700

    feat setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="scvi_rep",
+    description=(
+        "Tools for DE benchmarking"
+    ),
+    license="BSD",
+    packages=["R_interop", "scvi_utils"],
+)
