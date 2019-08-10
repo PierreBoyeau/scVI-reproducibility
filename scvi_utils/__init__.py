115cb713bca8b955a43a5b6ad1c90f1b370ff3c9
Pierre Boyeau <pierre.boyeau@gmail.com>
2019-08-09 18:12:57 -0700

    change init

--- scvi_utils/__init__.py
+++ scvi_utils/__init__.py
@@ -1,7 +1,8 @@
-from .lfc import estimate_de_proba, estimate_lfc_density, estimate_lfc_mean
+from .lfc import estimate_de_proba, estimate_lfc_density, estimate_lfc_mean, train_model
 
 __all__ = [
     "estimate_lfc_density",
     "estimate_lfc_mean",
-    "estimate_de_proba"
+    "estimate_de_proba",
+    "train_model"
 ]
